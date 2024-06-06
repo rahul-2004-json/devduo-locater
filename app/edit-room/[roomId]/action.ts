6a76d94e75ee1983aa3d1ce1b3409687a6e8cda7
rahul_004 <rahulbky2004@gmail.com>
2024-06-06 14:50:50 +0530

    Add edit room page , update the action file for create room and edit room

--- app/edit-room/[roomId]/action.ts
+++ app/edit-room/[roomId]/action.ts
@@ -1,19 +1,29 @@
 //this function can be envoked in the client side and run the server side backend code
 "use server";
-import { createRoom } from "@/data-access/rooms";
 import { Room} from "@/db/schema";
 import { getSession } from "@/lib/auth";
 import { revalidatePath } from "next/cache";
+import { editRoom,getRoom } from "@/data-access/rooms";
+import { redirect } from "next/navigation";
 
-export async function createRoomAction(roomData: Omit<Room, "id" | "userId">) {
+export async function editRoomAction(roomData: Omit<Room ,"userId">){
   const session = await getSession();
 
   if (!session) {
     throw new Error("You must be logged in to create this room");
   }
 
-  await createRoom(roomData,session.user.id);
+  const room=await getRoom(roomData.id);
+    if(room?.userId !==session.user.id){
+        throw new Error("User not authenticated");
+  }
 
+  await editRoom({...roomData, userId: room.userId });
+ 
   //Here revalidatePath clears the cache of the particular path and gives the fresh copy of everything when someone visits the given path
-  revalidatePath("/");
+  revalidatePath("/your-rooms");
+  revalidatePath(`/edit-room/${roomData.id}`);
+  redirect("/your-rooms");
 }
+
+
